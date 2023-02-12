9a35b988b2b8608e30bdfa9a12bb03e01df67c62
Denilson Lima <denilsondslima@gmail.com>
2023-02-12 16:53:42 -0300

    fix: correção

--- src/schemas/customersSchemas.js
+++ src/schemas/customersSchemas.js
@@ -1,8 +1,8 @@
 import joi from "joi"
 
 export const Schema = joi.object({
-    name: joi.string().required,
+    name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().pattern(/^\d+$/).required(),
+    cpf: joi.string().min(11).max(11).pattern(/^\d+$/).required(),
     birthday: joi.string().pattern(/^\d{4}\-\d{2}\-\d{2}$/).required()
 })
