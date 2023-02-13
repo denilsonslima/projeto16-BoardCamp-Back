08c3876da7981927bbfc54d648c5638db10260d8
Denilson Lima <denilsondslima@gmail.com>
2023-02-13 07:13:45 -0300

    teste

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -87,7 +87,11 @@ export const finalizarAluguel = async (req, res) => {
 
         if(idExiste.rows.length === 0) return res.sendStatus(404);
 
-        if(idExiste.rows[0].returnDate != null) return res.sendStatus(400)
+        const teste = await db.query(`
+        select * from rentals where id=$1 AND "returnDate" is null;
+        `, [id])
+
+        if(teste.rowCount !== 1) return res.sendStatus(400)
 
         const diaEntrega = dayjs(idExiste.rows[0].rentDate).format("DD")
         const delay = (dia - diaEntrega) * idExiste.rows[0].pricePerDay
