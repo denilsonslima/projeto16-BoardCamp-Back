146f1e126834118f136b35f207617ef114470921
Denilson Lima <denilsondslima@gmail.com>
2023-02-12 21:35:52 -0300

    feat: get rota rentals

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -2,7 +2,37 @@ import { db } from "../config/database.js"
 import dayjs from "dayjs"
 
 export const listarAlugueis = async (req, res) => {
+    try {
+        const dados = await db.query(`
+        SELECT json_build_object(
+            'id', r.id,
+            'customerId', r."customerId",
+            'gameId', r."gameId",
+            'rentDate', r."rentDate",
+            'daysRented', r."daysRented",
+            'returnDate', r."returnDate",
+            'originalPrice', r."originalPrice",
+            'delayFee', r."delayFee",
+            'customer', json_build_object(
+                'id', customers.id,
+                'name', customers.name
+            ),
+            'game', json_build_object(
+            'id', games.id,
+            'name', games.name)
+        )
+        FROM rentals r
+        JOIN customers
+            ON customers.id = r."customerId"
+        JOIN games
+            ON games.id = r."gameId";
+        `)
 
+        res.send(dados.rows)
+        
+    } catch (error) {
+        res.sendStatus(500)
+    }
 }
 
 export const adicionarAluguel = async (req,res) => {
--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { adicionarAluguel } from "../controllers/rentals.controller.js";
+import { adicionarAluguel, listarAlugueis } from "../controllers/rentals.controller.js";
 import { validateSchema } from "../middlewares/validateSchemas.js";
 import { Schema } from "../schemas/rentalsSchemas.js";
 
 const router = Router()
 
+router.get("/rentals", listarAlugueis)
 router.post("/rentals", validateSchema(Schema), adicionarAluguel)
 
 export default router
